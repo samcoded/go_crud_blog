930a196884d0497eac44c8a17f2a158ac29318b0
Samuel Anozie <samuelanozie10@yahoo.com>
2023-04-14 19:59:48 +0100

    feat: main entry point for app

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/samcoded/go_crud_blog/controllers"
+	"github.com/samcoded/go_crud_blog/initializers"
+)
+
+func init() {
+	initializers.LoadEnv()
+	initializers.ConnectDB()
+
+}
+
+func main() {
+	r := gin.Default()
+	r.POST("/posts", controllers.PostsCreate)
+	r.GET("/posts", controllers.PostsIndex)
+	r.GET("/posts/:id", controllers.PostsShow)
+	r.PUT("/posts/:id", controllers.PostsUpdate)
+	r.DELETE("/posts/:id", controllers.PostsDelete)
+	r.Run() // listen and serve on 0.0.0.0:3000
+}
